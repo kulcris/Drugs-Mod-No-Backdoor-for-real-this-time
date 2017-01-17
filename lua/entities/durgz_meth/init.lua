0b905d0049222a69a82fc0ad21c75747ecbf977a
kulcris <browncjay@gmail.com>
2017-01-16 23:02:23 -0600

    Added files

---
+++ lua/entities/durgz_meth/init.lua
@@ -0,0 +1,40 @@
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+ENT.MODEL = "models/katharsmodels/contraband/metasync/blue_sky.mdl"
+
+ENT.MASS = 2;
+
+ENT.LASTINGEFFECT = 30; --how long the high lasts in seconds
+
+function ENT:High(activator,caller)
+
+	sound.Play("drugs/insufflation.wav", activator:GetPos(), 75, 100, 1)
+	self:Say(activator, "WHOO! YEAH!! FUCK YEAH! I FEEL GREAT!! I FEEL ALIVE!!")	
+
+    if not self:Realistic() then
+        activator:SetHealth(activator:Health()/3)
+    end
+	
+	activator:SetGravity(1.5);	
+
+
+end
+
+function ENT:AfterHigh(activator, caller)
+	if( activator:Health() == 1 ) or ( activator:Health() == 2 ) then
+		activator.DURGZ_MOD_DEATH = "durgz_meth";
+		activator.DURGZ_MOD_OVERRIDE = activator:Nick().." died of a heart attack (methamphetamine overdose).";
+		activator:Kill()
+	return
+	end
+end
+
+--set speed back to normal once your high is over 
+hook.Add("Think", "durgz_meth_resetspeed", function()
+    for id,pl in pairs(player.GetAll())do
+        if pl:GetNetworkedFloat("durgz_meth_high_end") < CurTime() then
+			pl:SetGravity(1)
+        end
+    end
+end)
